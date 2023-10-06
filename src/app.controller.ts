7e15bd4a2cc2d1870aa8a8bba2b1074bd228e8c4
mykytazviahin <mykytazviahin@gmail.com>
2023-10-06 13:42:42 +0200

    feat: add swagger docs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,25 @@
 import { Controller, Get, Query } from '@nestjs/common';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiQuery,
+} from '@nestjs/swagger/dist';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiTags('cities')
   @Get('/cities')
+  @ApiOperation({ summary: 'Get cities population' })
+  @ApiResponse({
+    status: 200,
+    description: 'The information about cities',
+  })
+  @ApiQuery({ name: 'cityFilter', type: String })
+  @ApiQuery({ name: 'groupByFirstName', type: Boolean })
   getCities(@Query() query): Promise<any> {
     return this.appService.getCities(query.cityFilter, query.groupByFirstName);
   }
